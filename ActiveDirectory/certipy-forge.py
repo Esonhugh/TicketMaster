a3284aaeae52365979a60f8c9975bff6945f191c
Esonhugh <esonhughoutside@gmail.com>
2024-01-03 20:49:24 +0800

    index: certipy forge golden certificate in Active Directory

---
+++ ActiveDirectory/certipy-forge.py
@@ -0,0 +1,4 @@
+# command entry: https://github.com/ly4k/Certipy/blob/main/certipy/commands/forge.py 
+# certificate lib: https://github.com/ly4k/Certipy/blob/main/certipy/lib/certificate.py
+
+print("Use ceritpy forge")
